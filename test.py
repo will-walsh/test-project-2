bd4697508d86e1eba6c3109b033babb9c35baa5f
Will Walsh <WillWalsh@MacBook-Pro-7.local>
2020-04-19 12:40:21 -0500

    testing print and if then

--- test.py
+++ test.py
@@ -9,3 +9,15 @@ print("x is equal to y: %s" % (x==y))
 z = 1
 print("x is equal to z: %s" % (x==z))
 
+course = 'Python for Beginners'
+print (course.lower())
+print (course.upper())
+print (course)
+shorter_course = course[0:6]
+print (shorter_course)
+if (shorter_course in course):
+    print("You will learn %s in %s" % (shorter_course, course))
+else:
+    print("You will not learn %s in %s" % (shorter_course, course))
+    
+
